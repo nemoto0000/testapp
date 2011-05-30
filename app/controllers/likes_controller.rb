035053010b1d613389efce8ac245b655f05dca32
Shinsuke Sugita <shinriyo@gmail.com>
2011-05-30 15:26:38 +0900

    cancel

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,15 @@ class LikesController < ApplicationController
   def index
     facebook_id = session[:facebook_id]
     session[:facebook_id] = facebook_id
+
+    if params[:cancel] != nil
+      meetings = Like.where("uid = ? and meetinguid = ?", facebook_id, params[:cancel])
+      for meeting in meetings
+        meeting.judge = false
+        meeting.save
+      end
+    end
+
     @user = User.find(:first, :conditions => ["uid = ?", facebook_id])
     @client = session[:client]
 
